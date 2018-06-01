10e707673550d2a88547c2030b13f15b1dec47f3
w0nd3r0us <steven.grewal@gmail.com>
2018-05-31 20:40:36 -0400

    started paperboy

---
+++ Paperboy/paperboy.rb
@@ -0,0 +1,32 @@
+class Paperboy
+
+  def initialize(name)
+    @name = name
+    @experience = 0
+    @earnings = 0
+  end
+
+  def quota
+  end
+
+  def deliver(start_address, end_address)
+  end
+
+  def earnings
+    @earnings =
+  end
+
+  def report
+  end
+
+end
+
+
+#
+#
+#
+#
+#
+#
+#
+#
